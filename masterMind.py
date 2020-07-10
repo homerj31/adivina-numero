1995ced2ba23e309e1370782bed75d4949d8df55
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2020-07-10 13:42:22 +0200

    Update masterMind.py

--- masterMind.py
+++ masterMind.py
@@ -1,38 +1,24 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
 import random 
-codigo="".join(list(map(lambda x: str(x), random.sample(range(0,9), 4))))
 
- 
+codigo="".join(list(map(lambda x: str(x), random.sample(range(0,9), 4))))
 
 print ("##### Bienvenido/a al Mastermind! ######\n")
 
 print ("Tienes que adivinar un numero de 4 cifras distintas")
 
-propuesta = raw_input("¿Que codigo propones?: ")
-
- 
-
-# bucle hasta que el numero introducido coincida con el codigo aleatorio
+propuesta = input("¿Que codigo propones?: ")
 
 intentos = 0
 
 while propuesta != codigo:
+ intentos += 1
+ aciertos = 0
+ coincidencias = 0
 
-    intentos += 1
-
-    aciertos = 0
-
-    coincidencias = 0
-
- 
-
-    # recorremos la propuesta y verificamos en el codigo
-
-    for i in range(4):
-
-        if propuesta[i] == codigo[i]:
+   for i in range(4):
+   if propuesta[i] == codigo[i]:
 
             aciertos = aciertos + 1
 
