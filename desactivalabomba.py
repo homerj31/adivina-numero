2b52361a9a33fd3563c69b8b0136f96b3ecac2c4
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2020-07-11 13:45:44 +0200

    Update desactivalabomba.py

--- desactivalabomba.py
+++ desactivalabomba.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-from  tkinter import *
+from  Tkinter import *
 import time
 
 root = Tk()
 root.title("Desactiva la bomba")
 root.geometry("490x650")
+
+Label(root,width=58,height=6,bg="black").place(x=11,y=23)
 
 root.mainloop()
